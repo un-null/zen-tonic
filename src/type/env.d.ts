b90738bf05b1b78e639a61bbe77f4b2935eeaa11
null <keita@gaogao.asia>
2024-02-03 21:47:28 +0900

    🏷️ env.d.ts #5

---
+++ src/type/env.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+    CLERK_SECRET_KEY: string;
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: string;
+
+    NGROK_WEBHOOK_SECRET: string;
+  }
+}
