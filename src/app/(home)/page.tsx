0479c68b6bb356f21388209d142b96ec94f43629
null <keita@gaogao.asia>
2024-02-19 22:33:49 +0900

    ✨ impl basic loading ui #20

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
+
 import { currentUser } from "@clerk/nextjs";
 import { Avatar, Box, Card, Flex, TabsPanel, Text } from "@mantine/core";
 
 import { prisma } from "@/lib/prisma";
 
 import Tab from "./home-tab";
+import Loading from "./loading";
 import NoPostCard from "./no-post-card";
 import PostMenu from "./post-menu";
 
@@ -31,47 +34,54 @@ export default async function Home() {
   });
 
   const projectTitleArr = projects.map((project) => project.title);
+
   return (
     <Tab>
-      <Box my={40}>
-        <TabsPanel value="all">
-          {!userLatestPost ? (
-            <NoPostCard projects={projectTitleArr} />
-          ) : userLatestPost.created_at.getDate() !== new Date().getDate() ? (
-            <NoPostCard projects={projectTitleArr} />
-          ) : null}
+      <Suspense fallback={<Loading />}>
+        <Box my={40}>
+          <TabsPanel value="all">
+            {!userLatestPost ? (
+              <NoPostCard projects={projectTitleArr} />
+            ) : userLatestPost.created_at.getDate() !== new Date().getDate() ? (
+              <NoPostCard projects={projectTitleArr} />
+            ) : null}
 
-          {AllPosts.map((post) => (
-            <Card
-              key={post.id}
-              mx={"auto"}
-              padding="lg"
-              radius={0}
-              style={{
-                borderBottom: "1px solid #C9C9C9",
-              }}
-            >
-              <Flex gap={16}>
-                <Avatar size={"md"} radius={"sm"} src={user?.imageUrl}></Avatar>
+            {AllPosts.map((post) => (
+              <Card
+                key={post.id}
+                mx={"auto"}
+                padding="lg"
+                radius={0}
+                style={{
+                  borderBottom: "1px solid #C9C9C9",
+                }}
+              >
+                <Flex gap={16}>
+                  <Avatar
+                    size={"md"}
+                    radius={"sm"}
+                    src={user?.imageUrl}
+                  ></Avatar>
 
-                <Flex direction={"column"} gap={8} flex={1}>
-                  <Flex align={"center"}>
-                    <Text flex={1} size="sm" c={"dimmed"}>
-                      {"@" + user?.firstName}
-                    </Text>
-                    <PostMenu postId={post.id} />
+                  <Flex direction={"column"} gap={8} flex={1}>
+                    <Flex align={"center"}>
+                      <Text flex={1} size="sm" c={"dimmed"}>
+                        {"@" + user?.firstName}
+                      </Text>
+                      <PostMenu postId={post.id} />
+                    </Flex>
+
+                    <Text size="sm">{post.content}</Text>
                   </Flex>
-
-                  <Text size="sm">{post.content}</Text>
                 </Flex>
-              </Flex>
-            </Card>
-          ))}
-        </TabsPanel>
-        <TabsPanel value="friends">
-          <NoPostCard projects={projectTitleArr} />
-        </TabsPanel>
-      </Box>
+              </Card>
+            ))}
+          </TabsPanel>
+          <TabsPanel value="friends">
+            <NoPostCard projects={projectTitleArr} />
+          </TabsPanel>
+        </Box>
+      </Suspense>
     </Tab>
   );
 }
