52b4332692b306738564e895eb367d29b0436f85
null <keita@gaogao.asia>
2024-03-21 20:41:51 +0900

    ✨ build friend's posts page #32

--- src/app/(home)/friends/page.tsx
+++ src/app/(home)/friends/page.tsx
@@ -1,7 +1,27 @@
 import { redirect } from "next/navigation";
 
 import { currentUser } from "@clerk/nextjs";
-import { Box } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Card,
+  Flex,
+  Group,
+  Avatar as MantineAvatar,
+  Text,
+  Title,
+} from "@mantine/core";
+import dayjs from "dayjs";
+import { CheckSquare2 } from "lucide-react";
+
+import { getFolloweeAllPosts, getUserLatestPosts } from "@/lib/db/post";
+import { getProjectTitles } from "@/lib/db/project";
+
+import Avatar from "../_components/avatar";
+import CraeteButton from "../_components/create-button";
+import LikeButton from "../_components/like-button";
+import NoPostCard from "../_components/no-post-card";
+import PostMenu from "../_components/post-menu";
 
 export default async function Home() {
   const user = await currentUser();
@@ -10,5 +30,118 @@ export default async function Home() {
     redirect("/sign-in");
   }
 
-  return <Box my={40}>Friends</Box>;
+  const followeesAllPosts = await getFolloweeAllPosts(user.id);
+
+  const [projectTitles, latestPost] = await Promise.all([
+    getProjectTitles(user.id),
+    getUserLatestPosts(user.id),
+  ]);
+
+  const projectTitleArr = projectTitles.map((project) => project.title);
+
+  const isPostedToday =
+    latestPost?.created_at.getDate() !== new Date().getDate();
+  return (
+    <Box my={20}>
+      {!latestPost ? (
+        <NoPostCard>
+          <CraeteButton type={"text"} projects={projectTitleArr} />
+        </NoPostCard>
+      ) : isPostedToday ? (
+        <NoPostCard>
+          <CraeteButton type={"text"} projects={projectTitleArr} />
+        </NoPostCard>
+      ) : null}
+
+      {followeesAllPosts.map((post) => (
+        <Card
+          key={post.id}
+          mx={"auto"}
+          padding="lg"
+          radius={0}
+          style={{
+            borderBottom: "1px solid #C9C9C9",
+          }}
+        >
+          <Flex gap={16}>
+            <Avatar
+              image={post.user.image}
+              title={post.project.title}
+              start={post.project.start_date}
+            >
+              <Flex justify={"space-between"}>
+                <Group gap={8}>
+                  <MantineAvatar
+                    size={32}
+                    radius={"sm"}
+                    src={post.user.image}
+                  />
+                  <Title order={3}>{post.user.name}</Title>
+                </Group>
+                {user.id !== post.user.id && (
+                  <Button size={"xs"} disabled>
+                    すでに友達です
+                  </Button>
+                )}
+              </Flex>
+            </Avatar>
+
+            <Flex direction={"column"} gap={8} flex={1}>
+              <Flex align={"center"}>
+                <Group flex={1} c={"dimmed"} gap={"xs"}>
+                  <Text size="sm">{`@${post.user.name}`}</Text>
+                  <Text size="xs">
+                    {dayjs(post.created_at).format("YYYY.MM.DD")}
+                  </Text>
+                </Group>
+
+                <PostMenu postId={post.id} />
+              </Flex>
+
+              <Text size="sm" style={{ whiteSpace: "pre-wrap" }}>
+                {post.content}
+              </Text>
+
+              <Flex
+                direction={"column"}
+                gap={4}
+                p={12}
+                style={{
+                  whiteSpace: "pre-wrap",
+                  border: "1px solid #C9C9C9",
+                  borderRadius: 4,
+                }}
+              >
+                <Text size="sm" fw={"bold"}>
+                  {post.project.title}
+                </Text>
+                <Group gap={4} mt={8} pl={2} c={"dimmed"}>
+                  <CheckSquare2 size={14} />
+                  <Text size="xs">Done</Text>
+                </Group>
+
+                {/* Fix spec  */}
+                <Box mt={4} ml={2}>
+                  <Text
+                    size="xs"
+                    py={3}
+                    px={6}
+                    bg={"#DDEBF1"}
+                    display={"inline"}
+                    style={{ borderRadius: 4 }}
+                  >
+                    {`${dayjs(post.created_at).diff(post.project.start_date, "day")}日継続中`}
+                  </Text>
+                </Box>
+              </Flex>
+
+              <Box mt={8}>
+                <LikeButton isLike={post.like.length !== 0} postId={post.id} />
+              </Box>
+            </Flex>
+          </Flex>
+        </Card>
+      ))}
+    </Box>
+  );
 }
--- src/lib/db/post.ts
+++ src/lib/db/post.ts
@@ -36,3 +36,47 @@ export const getAllPosts = () => {
     },
   });
 };
+
+export const getFolloweeAllPosts = async (userId: string) => {
+  const following = await prisma.user.findFirst({
+    where: { id: userId },
+    select: {
+      followees: {
+        select: {
+          followee_id: true,
+        },
+      },
+    },
+  });
+
+  const followingIds = following?.followees.map((user) => user.followee_id);
+
+  return await prisma.post.findMany({
+    where: {
+      user_id: {
+        in: followingIds ? [...followingIds, userId] : [userId],
+      },
+    },
+    orderBy: { created_at: "desc" },
+    select: {
+      id: true,
+      created_at: true,
+      content: true,
+      project: {
+        select: {
+          title: true,
+          start_date: true,
+        },
+      },
+      user: {
+        select: {
+          id: true,
+          name: true,
+          image: true,
+        },
+      },
+      // Fix get only first data  ???
+      like: true,
+    },
+  });
+};
